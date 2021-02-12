e835aa942f3db34de4a40587ed33c18ad03eb782
SivaramPg <sivarampg95@gmail.com>
2021-02-12 18:12:59 +0530

    updated root _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,57 @@
-import '../styles/globals.css'
+import dynamic from 'next/dynamic';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+const ReactTooltip = dynamic(() => import('react-tooltip'), { ssr: false });
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  @font-face {
+    font-family: 'Wotfard';
+    src: url('/wotfard-regular-webfont.eot');
+    src: url('/wotfard-regular-webfont.eot?#iefix') format('embedded-opentype'),
+    url('/wotfard-regular-webfont.ttf') format('truetype'),
+    url('/wotfard-regular-webfont.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'geomanist' ;
+    src: url('/geomanist-regular-webfont.eot');
+    src: url('/geomanist-regular-webfont.eot?#iefix') format('embedded-opentype'),
+    url('/geomanist-regular-webfont.woff') format('woff'),
+    url('/geomanist-regular-webfont.ttf') format('truetype'),
+    url('/geomanist-regular-webfont.woff2') format('woff2'),
+    url('/geomanist-regular-webfont.svg#webfont') format('svg');
+  }
+
+  body {
+    box-sizing: border-box;
+    font-family: 'geomanist', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const theme = {};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <ReactTooltip effect="solid" place="top" type="dark" />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
