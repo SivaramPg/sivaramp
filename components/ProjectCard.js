c56ea6b2cfe595974e8cecbac5b6939927e6b446
SivaramPg <sivarampg95@gmail.com>
2021-02-19 22:08:33 +0530

    project card component

---
+++ components/ProjectCard.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ProjectCardWrapper = styled.div``;
+
+export default function ProjectCard() {
+  return <ProjectCardWrapper></ProjectCardWrapper>;
+}
