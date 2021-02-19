d0b5d57f66620dd1838ff16305cbe32d5ea14ddf
SivaramPg <sivarampg95@gmail.com>
2021-02-19 22:05:51 +0530

    contact section component

---
+++ components/ContactSection.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import ContactForm from './ContactForm';
+import Container from './Container';
+import SectionTitle from './SectionTitle';
+
+const ContactSectionWrapper = styled.section`
+  width: 100%;
+  /* height: 100vh; */
+  min-height: 800px;
+  background: linear-gradient(
+    -114deg,
+    rgba(48, 182, 160, 0.1),
+    rgba(48, 191, 171, 0.6)
+  );
+
+  h1 {
+    width: 100%;
+  }
+
+  & > div {
+    display: flex;
+    justify-content: center;
+
+    .form-section,
+    .illustration-section {
+      width: 50%;
+      margin-top: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .illustration {
+        width: 600px;
+        height: 450px;
+      }
+    }
+  }
+`;
+
+export default function ContactSection() {
+  return (
+    <ContactSectionWrapper>
+      <Container style={{ alignItems: 'center' }}>
+        <SectionTitle>Contact Me</SectionTitle>
+        <section className="illustration-section">
+          <img className="illustration" src="/contact-me.svg" alt="" />
+        </section>
+        <section className="form-section">
+          <ContactForm />
+        </section>
+      </Container>
+    </ContactSectionWrapper>
+  );
+}
