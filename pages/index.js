8be414ff165231aaa94b5d5fa882dcb116779169
SivaramPg <sivarampg95@gmail.com>
2021-02-21 10:58:06 +0530

    bring in IBM Plex Mono fonts from Google Fonts

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ export default function Home() {
       <Head>
         <title>Sivaram's Website</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono,wght@0,400;0,700;0,500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Navbar />
       <HeroSection />
